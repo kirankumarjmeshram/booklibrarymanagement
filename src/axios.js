641434376376f52f61ddb967285d8790d6a5f649
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2025-07-13 21:21:44 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL,
-  baseURL:"https://librarymanagementserver-07f5.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true
 });
 
